7680aeba1dafdcfe080ab862a673fc8d2ac2d32c
Shivank19 <shivank.sapra@gmail.com>
2025-11-11 08:46:03 -0600

    base app

---
+++ src/views/TestParam.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+
+const route = useRoute();
+const paramId = ref(route.params.id);
+</script>
+
+<template>
+  <div>
+    <h1>Parameter Detail</h1>
+    <p>Parameter ID: {{ paramId }}</p>
+  </div>
+</template>
